fbfc63ee7f93f4c02db3be3d557f4cc1b8510967
lxw <826982262@qq.com>
2022-12-06 23:32:44 +0800

    init

---
+++ src/main/java/com/nev/login/NevLoginApplication.java
@@ -0,0 +1,13 @@
+package com.nev.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NevLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nev/login/NevLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nev.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NevLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
